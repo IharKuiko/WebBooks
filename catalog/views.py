dc9b96c2df7af5ca82a492b1dd32314890785afe
Kuiko Ihar <coooll81@gmail.com>
2023-05-11 22:31:37 +0200

    add base and index.html

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Book, Author, BookInstance, Genre
+from django.views import generic
 
 def index(request):
-    return HttpResponse("Main page World books")
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact=2).count()
+    num_authors = Author.objects.count()
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        # 'num_visits': num_visits
+    }
+    return render(request, 'index.html', context=context)
 
 
+class BookListView(generic.ListView):
+    model = Book
